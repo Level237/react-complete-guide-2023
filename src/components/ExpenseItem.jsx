18c9ff938bd3dc605e815381104e68585b508450
level237 <Bramslevel129@gmail.com>
2023-09-22 13:01:34 +0100

    init react complete guide

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,17 @@
+
+function ExpenseItem(){
+
+    return (
+        <>
+        <div>
+           March 27th 2020
+        </div>
+        <div>
+            <h2>Car Insurance</h2>
+            <div>$267</div>
+        </div>
+        </>
+    );
+}
+
+export default ExpenseItem;
