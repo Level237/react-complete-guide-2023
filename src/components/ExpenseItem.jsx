a72b7b5e09b9b0d7e6e629142c313cd2d5afe220
level237 <Bramslevel129@gmail.com>
2023-09-23 09:52:10 +0100

    Adding normal JavaScript Logic to Components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,17 @@
 import "./ExpenseItem.css"
 function ExpenseItem({date,amount,title}){
 
-    const expenseDate=new Date(2021,2,28);
-    const expenseTitle="Car Insurance"
-    const expenseAmount=267
+    const month=date.toLocaleString('en-US',{month:"long"})
+    const day=date.toLocaleString('en-US',{day:"2-digit"})
+    const year=date.getFullYear()
     return (
         <div className="expense-item">
+
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
         <div>
           {date.toISOString()}
         </div>
