a6b8219e7596463709b87cb97e2026e372938651
level237 <Bramslevel129@gmail.com>
2023-09-23 10:13:12 +0100

    AddiSplitting Components Into Multiple Components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,20 +1,12 @@
 import "./ExpenseItem.css"
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem({date,amount,title}){
 
-    const month=date.toLocaleString('en-US',{month:"long"})
-    const day=date.toLocaleString('en-US',{day:"2-digit"})
-    const year=date.getFullYear()
+  
     return (
         <div className="expense-item">
 
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
-        <div>
-          {date.toISOString()}
-        </div>
+            <ExpenseDate date={date}/>
         <div className="expense-item__description">
             <h2>{title}</h2>
             <div className="expense-item__price">${amount}</div>
