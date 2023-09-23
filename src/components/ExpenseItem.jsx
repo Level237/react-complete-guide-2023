f0f21aced602760217c9bc1da3bb3d8eb00af6a5
level237 <Bramslevel129@gmail.com>
2023-09-23 09:42:45 +0100

    Alternative Ways of Passing & Receiving  Handling props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,5 +1,5 @@
 import "./ExpenseItem.css"
-function ExpenseItem(props){
+function ExpenseItem({date,amount,title}){
 
     const expenseDate=new Date(2021,2,28);
     const expenseTitle="Car Insurance"
@@ -7,11 +7,11 @@ function ExpenseItem(props){
     return (
         <div className="expense-item">
         <div>
-          {props.date.toISOString()}
+          {date.toISOString()}
         </div>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
+            <h2>{title}</h2>
+            <div className="expense-item__price">${amount}</div>
         </div>
         </div>
     );
