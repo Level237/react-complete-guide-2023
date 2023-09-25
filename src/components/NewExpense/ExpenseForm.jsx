3b2eddaba30b8487cf992c07a89dc04fbe68dd32
level237 <Bramslevel129@gmail.com>
2023-09-25 14:24:43 +0100

    Updating State That Depends On The Previous State

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -2,29 +2,31 @@ import { useState } from "react";
 import "./ExpenseForm.css"
 const ExpenseForm=()=>{
 
-    //const [title,setTitle]=useState("")
-    //const [amount,setAmount]=useState("")
-    //const [date,setDate]=useState("")
+    const [title,setTitle]=useState("")
+    const [amount,setAmount]=useState("")
+    const [date,setDate]=useState("")
 
-    const [userInput,setUserInput]=useState({
-        title:"",
-        date:"",
-        amount:""
-    })
+    //const [userInput,setUserInput]=useState({
+        //title:"",
+       // date:"",
+       // amount:""
+    //})
     const titleChangehandler=(event)=>{
-        //setTitle(event.target.value)
-setUserInput({...userInput,title:event.target.value});
+        setTitle(event.target.value)
+//setUserInput({...userInput,title:event.target.value});
+//setUserInput((prevInput)=>{console.log(prevInput);return {...prevInput,title:event.target.value}})
     }
     const amountChangehandler=(event)=>{
-        setUserInput({...userInput,amount:event.target.value});
+        setAmount(event.target.value)
+        //setUserInput({...userInput,amount:event.target.value});
     }
     const dateChangehandler=(event)=>{
-        setUserInput({...userInput,date:event.target.value});
+        setDate(event.target.value)
+        //setUserInput({...userInput,date:event.target.value});
     }
     return (
         <form>
-    <div className="new-expense__controls">
-        <div className="new-expense__control">
+    <div className="new-expense__controls">        <div className="new-expense__control">
             <label>Title</label>
             <input type="text" onChange={titleChangehandler}/>
         </div>
