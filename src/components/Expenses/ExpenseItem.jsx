5c352ef3d83c09145290b63425d6dd53df6e96f7
level237 <Bramslevel129@gmail.com>
2023-09-26 16:03:38 +0100

    Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,23 +4,16 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../Ui/Card";
 const ExpenseItem=(props)=>{
 
-    const [title,setTitle]=useState(props.title);
-console.log("test");
 
-    const clickHandler=()=>{
-        setTitle("updated")
-        console.log("clicked");
-    }
-  
     return (
         <Card className="expense-item">
 
             <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
+       
         </Card>
     );
 }
