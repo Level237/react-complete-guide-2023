c1ba399fb00449c3903d0684f98c53fc5e816512
level237 <Bramslevel129@gmail.com>
2023-09-27 11:31:34 +0100

    Rendering Lists of Data

--- src/components/Expenses/Expense.jsx
+++ src/components/Expenses/Expense.jsx
@@ -14,9 +14,9 @@ const Expense=(props)=>{
     return(
         <Card className="expenses">
             <ExpenseFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler}/>
- <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
- <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
- <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
+ {props.items.map((item)=><ExpenseItem title={item.title} amount={item.amount} date={item.date}/>)}
+ 
+
         </Card>
     )
 }
