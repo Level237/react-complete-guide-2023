845bcd6c65aad196b94e979da4fc7a1ac45e0d93
level237 <Bramslevel129@gmail.com>
2023-09-27 11:48:06 +0100

    Using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Expense from './components/Expenses/Expense';
 import NewExpense from './components/NewExpense/NewExpense';
+
+const DUMMY_EXPENSES=[
+  {
+    id:"e1",
+    title:"Toilet Paper",
+    amount:68,
+    date:new Date(2021,2,28)
+  },
+  {
+    id:"e2",
+    title:"new Tv",
+    amount:96.7,
+    date:new Date(2023,2,28)
+  },
+  {
+    id:"e3",
+    title:"Car Insurance",
+    amount:206.5,
+    date:new Date(2022,5,12)
+  },
+  {
+    id:"e4",
+    title:"new Desk",
+    amount:100.7,
+    date:new Date(2020,7,14)
+  },
+]
 const App=() =>{
-  const expenses=[
-    {
-      id:"e1",
-      title:"Toilet Paper",
-      amount:68,
-      date:new Date(2021,2,28)
-    },
-    {
-      id:"e2",
-      title:"new Tv",
-      amount:96.7,
-      date:new Date(2023,2,28)
-    },
-    {
-      id:"e3",
-      title:"Car Insurance",
-      amount:206.5,
-      date:new Date(2022,5,12)
-    },
-    {
-      id:"e4",
-      title:"new Desk",
-      amount:100.7,
-      date:new Date(2020,7,14)
-    },
-  ]
+  
+  const [expenses,setExpenses]=useState(DUMMY_EXPENSES)
     /*
  return React.createElement(
   'div',
@@ -38,7 +41,9 @@ const App=() =>{
  )*/
 
  const addExpenseHandler=(expense)=>{
-  console.log(expense)
+  setExpenses(prevExpenses=>{
+    return [expense,...prevExpenses]
+  })
  }
   return (
     <div>
