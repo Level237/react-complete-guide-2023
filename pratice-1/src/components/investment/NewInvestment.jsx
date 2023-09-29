dce15a3de6abb292a1ca2bc054def80634dba83b
level237 <Bramslevel129@gmail.com>
2023-09-29 16:03:28 +0100

    Lifting the State Up

--- pratice-1/src/components/investment/NewInvestment.jsx
+++ pratice-1/src/components/investment/NewInvestment.jsx
@@ -7,36 +7,11 @@ const initialInput={
     'expected-return':7,
     duration:10
 }
-const NewInvestment=()=>{
+const NewInvestment=(props)=>{
 
 
     const [userInput,setUserInput]=useState(initialInput)
-    const calculateHandler = (userInput) => {
-        // Should be triggered when form is submitted
-        // You might not directly want to bind it to the submit event on the form though...
     
-        const yearlyData = []; // per-year results
-    
-        let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-        const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-        const expectedReturn = +userInput['expected-return'] / 100;
-        const duration = +userInput['duration'];
-    
-        // The below code calculates yearly results (total savings, interest etc)
-        for (let i = 0; i < duration; i++) {
-          const yearlyInterest = currentSavings * expectedReturn;
-          currentSavings += yearlyInterest + yearlyContribution;
-          yearlyData.push({
-            // feel free to change the shape of the data pushed to the array!
-            year: i + 1,
-            yearlyInterest: yearlyInterest,
-            savingsEndOfYear: currentSavings,
-            yearlyContribution: yearlyContribution,
-          });
-        }
-    
-        // do something with yearlyData ...
-      };
 
       const inputChangeHandler=(identifier,value)=>{
         setUserInput((prevInput)=>{
@@ -49,6 +24,7 @@ const NewInvestment=()=>{
 
       const submitHandler=(e)=>{
         e.preventDefault();
+        props.onCalculate(userInput)
       }
 
       const resetForm=()=>{
