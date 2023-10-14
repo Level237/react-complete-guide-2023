9ac225ccaf0c77240971b48dc6fa6d4b467b2732
level237 <Bramslevel129@gmail.com>
2023-10-14 20:18:26 +0100

    Adding the Cart Button Component

---
+++ Food-Order/src/components/Layout/HeaderCardButton.jsx
@@ -0,0 +1,20 @@
+import CardIcon from "../Cart/CartIcon"
+import classes from './HeaderCardButton.module.css'
+const HeaderCardButton=(props)=>{
+
+    return (
+        <>
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CardIcon/>
+            </span>
+            <span>Your Card</span>
+            <span className={classes.badge}>
+                3
+            </span>
+        </button>
+        </>
+    )
+}
+
+export default HeaderCardButton
