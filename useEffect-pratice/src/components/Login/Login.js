c11638820a71d62f8a086d6843da7c8702297833
level237 <Bramslevel129@gmail.com>
2023-10-14 14:17:55 +0100

    Diving into Forward Refs

--- useEffect-pratice/src/components/Login/Login.js
+++ useEffect-pratice/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useReducer, useContext } from 'react';
+import React, { useEffect, useState,useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -34,7 +34,9 @@ const Login = () => {
 
   const [emailState,dispatchEmail]=useReducer(emailReducer,{value:"",isValid:null})
   const [passwordState,dispatchPassword]=useReducer(passwordReducer,{value:"",isValid:null})
-
+  
+  const emailRef=useRef()
+  const passwordRef=useRef()
 
   const authCtx=useContext(AuthContext)
   useEffect(()=>{
@@ -78,7 +80,14 @@ const Login = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if(formIsValid){
+      authCtx.onLogin(emailState.value, passwordState.value);
+    }else if(!emailState.isValid){
+      emailRef.current.focus()
+    }else if(!passwordState.isValid){
+      passwordRef.current.focus()
+    }
+    
   };
 
   useEffect(()=>{
@@ -91,6 +100,7 @@ const Login = () => {
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input 
+        ref={emailRef}
         id="email" 
         label="E-mail" 
         type="email" 
@@ -100,6 +110,7 @@ const Login = () => {
         onBlur={validateEmailHandler}
         />
          <Input 
+         ref={passwordRef}
         id="password" 
         label="Password" 
         type="password" 
@@ -109,7 +120,7 @@ const Login = () => {
         onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
