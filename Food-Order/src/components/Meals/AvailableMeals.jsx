3c7f8c0b4f3896bf9d9f0eb3ace79b4923433064
level237 <Bramslevel129@gmail.com>
2023-10-15 11:58:47 +0100

    Adding Individual Meal Items & Displaying Them

--- Food-Order/src/components/Meals/AvailableMeals.jsx
+++ Food-Order/src/components/Meals/AvailableMeals.jsx
@@ -1,4 +1,6 @@
+import Card from "../Ui/Card";
 import classes from "./AvailableMeals.module.css"
+import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
     {
       id: 'm1',
@@ -28,13 +30,17 @@ const DUMMY_MEALS = [
 const AvailableMeals=(props)=>{
 
     const mealsList=DUMMY_MEALS.map((meal)=>{
-        return <li>{meal.name}</li>
+        return <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
     })
     return (
         <section className={classes.meals}>
+
+        <Card>
             <ul>
                 {mealsList}
             </ul>
+        </Card>
+            
         </section>
     )
 }
