08d49a4ac73e69417edfc6f71fd56617c0b295bf
level237 <Bramslevel129@gmail.com>
2023-10-16 17:24:21 +0100

    Adding a Cart Context

--- Food-Order/src/App.js
+++ Food-Order/src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import { CardProvider } from "./context/CartProvider";
 
 function App() {
 
@@ -15,13 +16,13 @@ function App() {
     setCardIsShow(false)
   }
   return (
-    <Fragment>
+    <CardProvider>
       {cardIsShow && <Cart onClose={hiddenCardHandler}/>}
       <Header onShowCard={showCardHandler}/>
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CardProvider>
   );
 }
 
---
+++ Food-Order/src/context/CartProvider.jsx
@@ -0,0 +1,24 @@
+import CartContext from "./cart-context"
+
+
+export const CardProvider=(props)=>{
+
+    const addItemToCartHandler=(item)=>{
+
+    }
+
+    const removeItemToCartHandler=(id)=>{
+
+    }
+
+    const cartContext={
+        items:[],
+        totalAmount:0,
+        addItem:addItemToCartHandler,
+        removeItem:removeItemToCartHandler
+    }
+    return <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+
+}
