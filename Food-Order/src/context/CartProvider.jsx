54c4a01a786c1d9d0ac96fc96985b10e7e28a7fa
level237 <Bramslevel129@gmail.com>
2023-10-17 10:16:58 +0100

    Working on a More Complex Reducer Logic

--- Food-Order/src/context/CartProvider.jsx
+++ Food-Order/src/context/CartProvider.jsx
@@ -8,7 +8,25 @@ const defaultCartState={
 }
 const cartReducer=(state,action)=>{
     if(action.type==="ADD"){
-        const updatedItems=state.items.concat(action.item);
+
+        const existingCartItemIndex=state.items.findIndex((item)=>{
+            return item.id===action.item.id
+        })
+        const existingCartItem=state.items[existingCartItemIndex]
+
+        let updatedItems;
+
+        if(existingCartItem){
+            const updateItem={
+                ...existingCartItem,
+                amount:existingCartItem.amount + action.item.amount
+            }
+            updatedItems=[...state.items]
+            updatedItems[existingCartItemIndex]=updateItem
+        }else{
+          updatedItems=state.items.concat(action.item);
+        }
+        
         const updatedTotalAmount=state.totalAmount + action.item.price * action.item.amount;
 console.log(updatedTotalAmount)
         return {
