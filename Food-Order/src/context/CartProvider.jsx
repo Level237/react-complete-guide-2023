56263730ccd9b9e5a7f56c2ac6b771892310c796
level237 <Bramslevel129@gmail.com>
2023-10-17 15:28:10 +0100

    Making Items Removable

--- Food-Order/src/context/CartProvider.jsx
+++ Food-Order/src/context/CartProvider.jsx
@@ -13,7 +13,6 @@ const cartReducer=(state,action)=>{
             return item.id===action.item.id
         })
         const existingCartItem=state.items[existingCartItemIndex]
-
         let updatedItems;
 
         if(existingCartItem){
@@ -21,6 +20,7 @@ const cartReducer=(state,action)=>{
                 ...existingCartItem,
                 amount:existingCartItem.amount + action.item.amount
             }
+            console.log(updateItem)
             updatedItems=[...state.items]
             updatedItems[existingCartItemIndex]=updateItem
         }else{
@@ -28,7 +28,31 @@ const cartReducer=(state,action)=>{
         }
         
         const updatedTotalAmount=state.totalAmount + action.item.price * action.item.amount;
-console.log(updatedTotalAmount)
+
+        return {
+            items:updatedItems,
+            totalAmount:updatedTotalAmount
+        }
+    }
+    if(action.type==="REMOVE"){
+
+        const existingCartItemIndex=state.items.findIndex((item)=>{
+            return item.id===action.id
+        })
+        const existingCartItem=state.items[existingCartItemIndex]
+        const updatedTotalAmount=state.totalAmount - existingCartItem.price
+        //console.log(updatedTotalAmount)
+        let updatedItems;
+        if(existingCartItem.amount===1){
+            updatedItems=state.items.filter((item)=>{
+                return item.id !==action.id
+            });
+        }else{
+            const updatedItem={...existingCartItem,amount:existingCartItem.amount - 1};
+            updatedItems=[...state.items];
+            updatedItems[existingCartItemIndex]=updatedItem
+        }
+
         return {
             items:updatedItems,
             totalAmount:updatedTotalAmount
