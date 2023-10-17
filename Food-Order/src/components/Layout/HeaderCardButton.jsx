e9a45c4667263d65eda9c9f937f19fb2cf14e52b
level237 <Bramslevel129@gmail.com>
2023-10-17 15:53:01 +0100

    tnIsHighlighted

--- Food-Order/src/components/Layout/HeaderCardButton.jsx
+++ Food-Order/src/components/Layout/HeaderCardButton.jsx
@@ -1,17 +1,36 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import CardIcon from "../Cart/CartIcon"
 import classes from './HeaderCardButton.module.css'
 import CartContext from "../../context/cart-context"
 const HeaderCardButton=(props)=>{
 
+    const [btnIsHighlighted,setBtnIsHighlighted]=useState(false)
     const cartCtx=useContext(CartContext)
 
     const numberOfCart=cartCtx.items.reduce((currentNumber,item)=>{
         return currentNumber + item.amount
     },0);
+
+    const {items}=cartCtx
+    const btnClasses=`${classes.button} ${ btnIsHighlighted ? classes.bump : ""}`
+
+    useEffect(()=>{
+        if(cartCtx.items.length===0){
+            return;
+        }
+        setBtnIsHighlighted(true)
+
+        const timer=setTimeout(()=>{
+            setBtnIsHighlighted(false)
+        },300)
+
+        return ()=>{
+            clearTimeout(timer)
+        }
+    },[cartCtx.items])
     return (
         <>
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CardIcon/>
             </span>
