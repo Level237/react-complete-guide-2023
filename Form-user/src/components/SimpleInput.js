5e5c2908b38732a15543cdae4f9a3c276e8694d2
level237 <Bramslevel129@gmail.com>
2023-12-30 16:05:21 +0100

    Handling the was touched State

--- Form-user/src/components/SimpleInput.js
+++ Form-user/src/components/SimpleInput.js
@@ -10,6 +10,14 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value)
   }
 
+  const nameInputBlurHandler=()=>{
+    setEnteredNameTouched(true);
+    if(enteredName.trim()===""){
+      setEnteredNameIsValid(false)
+      return;
+    }
+  }
+
   const formSubmissionHandler=event=>{
     event.preventDefault();
     const enteredValue=nameInputRef.current.value;
@@ -35,7 +43,13 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+        <input 
+        ref={nameInputRef} 
+        type='text' 
+        id='name' 
+        onChange={nameInputChangeHandler} 
+        onBlur={nameInputBlurHandler}
+        value={enteredName} />
       {nameInputIsValid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
