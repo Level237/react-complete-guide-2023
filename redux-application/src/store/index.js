de45d59bd2084c817578cc07f9f405201ebc5f6a
level237 <Bramslevel129@gmail.com>
2024-01-09 08:13:23 +0100

    Connecting Redux Toolkit State

--- redux-application/src/store/index.js
+++ redux-application/src/store/index.js
@@ -1,58 +1,30 @@
-import {createStore} from "redux";
-import {createSlice} from "@reduxjs/toolkit"
+
+import {createSlice,configureStore} from "@reduxjs/toolkit"
 export const INCREMENT="increment";
 const initialState={counter:0,showCounter:true}
-const counterReducer=(state=initialState,action)=>{
 
-    createSlice({
-        name:"counter",
-        initialState,
-        reducers:{
-            increment(state){
-                state.counter++;
-            },
-            decrement(state){
-                state.counter--;
-            },
-            increase(state,action){
-                state.counter=state.counter+action.amount
-            },
-            toogleCounter(state){
-                state.showCounter=!state.showCounter
-            }
-        }
-    })
-    if(action.type===INCREMENT){
-        //state.counter++; You don't never mutate state
-        return {
-            counter:state.counter +1,
-            showCounter:state.showCounter
-        };
-    }
-
-    if(action.type==="increase"){
-        return {
-            counter:state.counter +action.amount,
-            showCounter:state.showCounter
-        };
-    }
-    if(action.type==="decrement"){
-        return {
-            counter:state.counter -1,
-            showCounter:state.showCounter
-        };
-    }
-
-    if(action.type==="toogle"){
-        return{
-            showCounter:!state.showCounter,
-            counter:state.counter
+const counterSlice=createSlice({
+    name:"counter",
+    initialState,
+    reducers:{
+        increment(state){
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        increase(state,action){
+            state.counter=state.counter+action.amount
+        },
+        toogleCounter(state){
+            state.showCounter=!state.showCounter
         }
     }
-   return state;
-};
+})
 
-const store=createStore(counterReducer);
+const store=configureStore({
+    reducer:counterSlice.reducer
+});
 
 
 export default store;
