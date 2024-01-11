77960657295af063a54ffb12cca1036b7425f9c3
level237 <Bramslevel129@gmail.com>
2024-01-11 14:22:19 +0100

    Using an Action Creator Thunk

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -1,23 +1,41 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useSelector} from "react-redux"
+import {useSelector,useDispatch} from "react-redux"
+import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial=true
 function App() {
+  const dispatch=useDispatch()
   const showCart=useSelector(state=>state.ui.cartIsVisible)
   const cart=useSelector((state)=>state.cart)
-
+  const notification=useSelector((state)=>state.ui.notification)
   useEffect(()=>{
-    fetch("https://food-15762-default-rtdb.firebaseio.com/cart.json",{
-      method:"PUT",
-      body:JSON.stringify(cart)
-    })
-  },[cart])
+    if(isInitial){
+      isInitial=false;
+      return;
+    }
+    dispatch(sendCartData(cart))
+  },[cart,dispatch])
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+       
+      )}
+       <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+   
   );
 }
 
