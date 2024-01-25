65801299df1fe1e6d0ba556443eebaf9c130a6dc
level237 <Bramslevel129@gmail.com>
2024-01-25 10:38:42 +0100

    Output a list of dummy events to the EventsPage //    Every list item should include a link to the respective EventDetailPage

--- router-project/frontend/src/pages/EventsPage.jsx
+++ router-project/frontend/src/pages/EventsPage.jsx
@@ -1,8 +1,28 @@
+import EventsList from "../components/EventsList"
+
+const EVENTS=[{
+    id:"e1",
+    title:"event1",
+    date:"12-05-2023"
+},{
+    id:"e2",
+    title:"event2",
+    date:"12-05-2023"
+},{
+    id:"e3",
+    title:"event3",
+    date:"12-05-2023"
+},{
+    id:"e4",
+    title:"event4",
+    date:"12-05-2023"
+}]
 const EventsPage=()=>{
+
 
     return (
         <>
-        <h1>Events Page</h1>
+        <EventsList events={EVENTS}/>
         </>
     )
 }
