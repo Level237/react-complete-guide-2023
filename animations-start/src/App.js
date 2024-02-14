a9d5ac8869c845279dbb4ecc70217537ae159a68
level237 <Bramslevel129@gmail.com>
2024-02-14 14:41:51 +0100

    Using the Transition Component

--- animations-start/src/App.js
+++ animations-start/src/App.js
@@ -41,7 +41,12 @@ class App extends Component {
           )}
        
         </Transition>
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
+        <Transition mountOnEnter
+        unmountOnExit in={this.state.modalIsOpen} timeout={300}>
+          {state=>(
+             <Modal show={state} closed={this.closeModal}/>
+          )}
+        </Transition>
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         
         <button onClick={this.showModal} className="Button">Open Modal</button>
