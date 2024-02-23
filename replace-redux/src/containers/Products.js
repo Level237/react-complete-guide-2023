7f9be7e5e43b88a5fc924288106df693c055925a
level237 <Bramslevel129@gmail.com>
2024-02-23 16:44:04 +0100

    Alternative Using the Context API

--- replace-redux/src/containers/Products.js
+++ replace-redux/src/containers/Products.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import { ProductContext } from '../context/Product-context';
+import { useContext } from 'react';
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+  const productList=useContext(ProductContext).products;
+  console.log(productList);
   return (
     <ul className="products-list">
       {productList.map(prod => (
--- replace-redux/src/index.js
+++ replace-redux/src/index.js
@@ -3,22 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import { ProductProvider } from './context/Product-context';
 import './index.css';
 import App from './App';
 import productReducer from './store/reducers/products';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
 
-const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ProductProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
+  </ProductProvider>,
   document.getElementById('root')
 );
