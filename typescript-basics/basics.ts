47766d6a1995b257babe7c12319db5ba7ae6fdf3
level237 <Bramslevel129@gmail.com>
2024-03-13 14:34:01 +0100

    Exploring the Base Types

---
+++ typescript-basics/basics.ts
@@ -0,0 +1,18 @@
+//Primitives:number,string,boolean
+//More complex:array,objects
+//Function types,parameters
+
+
+//primitives
+
+let age:number;
+
+age=12.1;
+
+let userName:string;
+
+userName='Max';
+
+let isInstructor:boolean;
+
+isInstructor=true
