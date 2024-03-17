8ca09f85ab3ac576ae7c22a1e337cfba793a4257
level237 <Bramslevel129@gmail.com>
2024-03-17 16:12:55 +0100

    Working with Components & TypeScript

---
+++ react-ts/src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Todos() {
+  return (
+    <div>
+      <ul>
+        <li>Learn React</li>
+        <li>Learn Typescript</li>
+      </ul>
+    </div>
+  )
+}
--- typescript-basics/with-typescript.ts
+++ typescript-basics/with-typescript.ts
@@ -2,6 +2,20 @@ function adds(a:number, b:number):number {
     return a + b;
   }
   
-  const result = add(2, 5);
+  const result = adds(2, 5);
   
   console.log(result);
+
+
+  //Generics
+
+  function insertAtBeginning<T>(array:T[],value:T){
+    const newArray=[value,...array];
+    return newArray;
+
+  }
+
+  const demoArray=[1,2,3]
+
+  const updatedArray=insertAtBeginning(demoArray,-1)
+  const stringArray=insertAtBeginning(["a","b","c"],"d")
