a7211ef6eb2a6686b5ddec6e3a0b2030ecac6810
level237 <Bramslevel129@gmail.com>
2024-03-18 13:59:58 +0100

    Working with refs & useRef

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Todos from './components/Todos';
 import Todo from './models/Todo';
+import NewTodo from './components/NewTodo';
 
 
 function App() {
   const todos=[new Todo('Learn React'),new Todo('Learn Typescript')];
   return (
     <div>
+      <NewTodo/>
      <Todos items={todos}/>
     </div>
   );
